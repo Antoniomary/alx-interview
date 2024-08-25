1c896bfcace585b6c0da4e7eea7f6b9fb97bcb2e
Antoniomary <amanso.totti@yahoo.com>
2024-08-25 19:50:18 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""contains the minOperations function"""
+import math
+
+
+def minOperations(n):
+    """returns an int that corresponds to the fewest number of operations
+       needed to result in exactly n H characters.
+    """
+    if n <= 1:
+        return 0
+ 
+    num_of_operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            num_of_operations += factor
+            n //= factor
+        factor += 1
+
+    return num_of_operations
