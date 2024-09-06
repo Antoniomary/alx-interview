0ea6435c57b414878fed9cf0e65fe245f540f2d8
Antoniomary <amanso.totti@yahoo.com>
2024-09-06 06:59:13 +0000

    fixed: condition to check for 10xxxxxx

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,7 +2,6 @@
 """contains validUTF8 function"""
 
 
-
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding.
        Returns True if data is a valid UTF-8 encoding, else False
@@ -14,16 +13,14 @@ def validUTF8(data):
     n_bytes = 0
     for n in data:
         if n_bytes == 0:
-            if n & masks[0] == 0b01000000:
+            if n & masks[0] == 0b00000000:
                 continue
-            elif n & masks[1] == 0b11000000:
-                n_bytes = 1
             elif n & masks[2] == 0b11100000:
                 n_bytes = 2
             elif n & masks[3] == 0b11110000:
                 n_bytes = 3
         else:
-            if not (n & 0b11000000 == 0b10000000):
+            if not (n & masks[1] == 0b10000000):
                 return False
             n_bytes -= 1
 
